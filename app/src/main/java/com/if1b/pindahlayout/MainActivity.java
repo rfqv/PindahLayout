f5ef66a04686a3e6eb09344a35c1446d3b42109c
YandD9 <rifqivirgiansyah@gmail.com>
2022-10-10 10:05:03 +0700

    update 101022

--- app/src/main/java/com/if1b/pindahlayout/MainActivity.java
+++ app/src/main/java/com/if1b/pindahlayout/MainActivity.java
@@ -25,8 +25,15 @@ public class MainActivity extends AppCompatActivity {
         btnPindah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent pindah = new Intent(MainActivity.this,SecondActivity.class);
-                startActivity(pindah);
+                String nama = etNama.getText().toString();
+                if(nama.trim().equals("")){
+                    etNama.setError("Nama Harus Diisi!");
+                }
+                else{
+                    Intent pindah = new Intent(MainActivity.this,SecondActivity.class);
+                    pindah.putExtra("xNama", nama);
+                    startActivity(pindah);
+                }
             }
         });
     }
